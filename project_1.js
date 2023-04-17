dd1317a1cb0395a66653ea1a760aad4b305d9cc5
LanaStrogaleva <lana.strogaleva@gmail.com>
2023-04-17 15:31:10 +0300

    commit 1

---
+++ project_1.js
@@ -0,0 +1,8 @@
+//my first project
+let coffeeType1 = "cappuchino";
+let coffeeType2 = "latte";
+let coffeeType3 = "americano";
+let topping1 = 'ice cream';
+let topping2 = 'milk';
+
+let price = 100
